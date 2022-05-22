78eb9f5c26c1aec3f4a1138ee68cc56e212999ff
DeltaJoy <j.komorowski1991@gmail.com>
2022-05-22 15:19:26 -0500

    calculator layout

---
+++ Calculator Clone/js.js
@@ -0,0 +1,19 @@
+function calculator() {
+    let integers = document.querySelectorAll(".int")
+    let operator = document.querySelectorAll(".opp")
+    let decimal = document.querySelector(".dec")
+    let percentage = document.querySelector(".per")
+    let clearButton = document.querySelector(".cl")
+    let equals = document.querySelector(".eq")
+    let negatives = document.querySelector(".neg")
+    let answer = document.querySelector(".ans")
+
+    let currentMath = ""
+    let currentNumber1 = ""
+    let currentNumber2 = ""
+
+    
+}
+
+
+console.log("hi")

f6641f314f2eb460a0dcabff9b22650054841e02
DeltaJoy <j.komorowski1991@gmail.com>
2022-05-23 06:56:38 -0500

    Operator eventListener. Restart Point.
    The code works at this point. I believe I can shorten up the code so if this doesn't work how I am hoping I want to come back to this point here.

--- Calculator Clone/js.js
+++ Calculator Clone/js.js
@@ -3,14 +3,11 @@
 //     let operator = document.querySelectorAll(".opp")
 //     let decimal = document.querySelector(".dec")
 //     let percentage = document.querySelector(".per")
-//     let clearButton = document.querySelector(".cl")
+    // let clearButton = document.querySelector(".cl")
 //     let equals = document.querySelector(".eq")
 //     let negatives = document.querySelector(".neg")
 //     let answer = document.querySelector(".ans")
 
-//     let currentMath = ""
-//     let currentNumber1 = ""
-//     let currentNumber2 = ""
 
 
 //     //adding event listner to each number 
@@ -31,9 +28,10 @@
     
 // }
 
+let integers = document.querySelectorAll(".int")
+const clearButton = document.getElementById("clear")
+const operator = document.querySelectorAll(".opp")
 
-// calculator()
-    let integers = document.querySelectorAll(".int")
 
 
 let addition = function (a, b) {
@@ -66,21 +64,12 @@ let operate = function (a,b, math) {
 
 
 
-let displayValue = function () {
 
 
-
-    // integers.forEach(number => {
-    //     number.addEventListener("click", () => {
-    //         document.getElementById("answerSection").innerText = number.innerText
-            
-    //     })
-    // })
-    
-}
 
 
 let num1Array = []
+let num2Array = []
 integers.forEach(number => {
     number.addEventListener("click", () => {
         
@@ -90,8 +79,55 @@ integers.forEach(number => {
             number.disabled = true
         }
         
+        // section 1//
     })
+
+    // moved this section of code down it was in sectin 1
+    clearButton.onclick = () => {
+        num1Array.length = 0
+        document.getElementById("answerSection").innerText = num1Array
+        
+    }
+
+
+    //logs the operator in the calculator and will store num1array 
+
+    operator.forEach(opp => {
+        opp.addEventListener("click", ()=> {
+
+        })
+
+        opp.onclick = () => {
+            
+            console.log(num1Array)
+            let joiningNum1Array = num1Array.join("")
+            let toIntNum1Array = parseInt(joiningNum1Array)
+            console.log(toIntNum1Array)
+
+            let operate = function (toIntNum1Array,b, math) {
+                if ( math == addition ) {
+                    console.log(a + b)
+                } else if ( math == subtraction )  {
+                    console.log(a - b)
+                } else if ( math == multiplication ) {
+                    console.log(a * b)
+                } else if ( math == division ) {
+                    console.log(a / b)
+                }
+            }
+        }
+        
+
+    })
+
 })
 
 
-document.getElementById("one").addEventListener("click", displayNumbers)
+
+let arr = [1,2,3]
+let joined = arr.join("")
+let int = parseInt(joined)
+console.log(typeof
+    (int))
+let sub = int - 2
+
